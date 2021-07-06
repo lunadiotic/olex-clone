bff9056368f3b37facd14c35d6eac42816503b45
Lionatic <ahmadhakim404@gmail.com>
2021-07-06 17:19:12 +0700

    membuat middleware cek token otentikasi

---
+++ src/middleware/authJwt.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const config = require('../config/auth')
+
+verifyToken = (req, res, next) => {
+  let token = req.headers['authorization']
+
+  if (!token) {
+    return res.status(403).json({
+      message: 'no token provided!',
+    })
+  }
+
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        message: 'unauthorized!',
+      })
+    }
+    req.userId = decoded.id
+    next()
+  })
+}
+
+module.exports = {
+  verifyToken,
+}
