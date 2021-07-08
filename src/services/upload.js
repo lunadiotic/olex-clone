97973acfaef6a2bb7e79beeab178a911173f301c
Lionatic <ahmadhakim404@gmail.com>
2021-07-08 16:53:03 +0700

    Membuat Service Upload File dengan Multer

---
+++ src/services/upload.js
@@ -0,0 +1,33 @@
+const multer = require('multer')
+const util = require('util')
+const path = require('path')
+const __basedir = path.resolve()
+
+const imageFilter = (req, file, cb) => {
+  if (file.mimetype.starsWith('image')) {
+    cb(null, true)
+  } else {
+    cb('please upload only image.', false)
+  }
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __basedir + '/storage/upload')
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`)
+  },
+})
+
+const uploadImage = multer({
+  storage: storage,
+  fileFilter: imageFilter,
+}).array('files', 5)
+
+let uploadFile = util.promisify(uploadImage)
+
+module.exports = {
+  uploadFile,
+  __basedir,
+}
