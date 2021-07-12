aab0d0aa3860590669dbe202a1dc7c6b61ead6a8
Lionatic <ahmadhakim404@gmail.com>
2021-07-12 10:51:54 +0700

    Menampilkan Data Produk berdasarkan Pencarian Judul dan Lokasi

--- src/controllers/ads.controller.js
+++ src/controllers/ads.controller.js
@@ -2,6 +2,48 @@ const db = require('../models')
 const Product = db.product
 const Image = db.image
 const User = db.user
+const Op = db.Sequelize.Op
+
+exports.search = (req, res) => {
+  const lat = parseFloat(req.query.lat)
+  const lng = parseFloat(req.query.lng)
+  const { title } = req.query
+  let condition = title ? { title: { [Op.like]: `%${title}%` } } : null
+
+  Product.findAll({
+    attributes: {
+      include: [
+        [
+          db.sequelize.literal(
+            `6371 *
+            acos(cos(radians(${lat})) * cos(radians(loc_latitude)) *
+            cos(radians(${lng}) - radians(loc_longitude)) +
+            sin(radians(${lat})) * sin(radians(loc_latitude)))`
+          ),
+          'distance',
+        ],
+      ],
+    },
+    where: {
+      sold: false,
+      ...condition,
+    },
+    order: db.sequelize.col('distance'),
+    limit: 10,
+    include: Image,
+  })
+    .then((result) => {
+      res.status(200).json({
+        data: result,
+        message: 'list products success',
+      })
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err.message,
+      })
+    })
+}
 
 exports.detail = (req, res) => {
   id = req.params.id
