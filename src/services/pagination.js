33826ceb0a0c9977b011d9da33db2938b5abf824
Lionatic <ahmadhakim404@gmail.com>
2021-07-12 14:45:21 +0700

    membuat service pagination

---
+++ src/services/pagination.js
@@ -0,0 +1,19 @@
+const getPagination = (page, size) => {
+  const limit = size ? +size : 3
+  const offset = page ? page * limit : 0
+
+  return { limit, offset }
+}
+
+const getPagingData = (result, page, limit) => {
+  const { count: totalItems, rows: data } = result
+  const currentPage = page ? +page + 1 : 0
+  const totalPages = Math.ceil(totalItems / limit)
+
+  return { totalItems, data, totalPages, currentPage }
+}
+
+module.exports = {
+  getPagination,
+  getPagingData,
+}
